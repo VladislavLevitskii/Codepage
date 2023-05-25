d6731a398839e8c278d37ff0140b9418cb4a5d62
Vladislav Levitskii <vladlevitskii@gmail.com>
2023-05-25 15:46:28 +0200

    15:46

---
+++ JavaScript/náhradnídíly.js
@@ -0,0 +1,98 @@
+const item1 = document.querySelector(".priceandarrow1");
+const description1 = document.querySelector(".description1");
+const arrow1 = document.querySelector(".arrow1");
+function toggleMenu1() {
+    if (description1.classList.contains("showMenu")) {
+    description1.classList.remove("showMenu")
+    arrow1.style.transform = "rotate(0deg)";
+    }   else {
+        description1.classList.add("showMenu")
+        arrow1.style.transform = "rotate(180deg)";
+        }
+}
+item1.addEventListener("click", toggleMenu1);
+
+const item2 = document.querySelector(".priceandarrow2");
+const description2 = document.querySelector(".description2");
+const arrow2 = document.querySelector(".arrow2");
+function toggleMenu2() {
+    if (description2.classList.contains("showMenu")) {
+    description2.classList.remove("showMenu")
+    arrow2.style.transform = "rotate(0deg)";
+    }   else {
+        description2.classList.add("showMenu")
+        arrow2.style.transform = "rotate(180deg)";
+        }
+}
+item2.addEventListener("click", toggleMenu2);
+
+
+const item3 = document.querySelector(".priceandarrow3");
+const description3 = document.querySelector(".description3");
+const arrow3 = document.querySelector(".arrow3");
+function toggleMenu3() {
+    if (description3.classList.contains("showMenu")) {
+    description3.classList.remove("showMenu")
+    arrow3.style.transform = "rotate(0deg)";
+    }   else {
+        description3.classList.add("showMenu")
+        arrow3.style.transform = "rotate(180deg)";
+        }
+}
+item3.addEventListener("click", toggleMenu3);
+
+const item4 = document.querySelector(".priceandarrow4");
+const description4 = document.querySelector(".description4");
+const arrow4 = document.querySelector(".arrow4");
+function toggleMenu4() {
+    if (description4.classList.contains("showMenu")) {
+    description4.classList.remove("showMenu")
+    arrow4.style.transform = "rotate(0deg)";
+    }   else {
+        description4.classList.add("showMenu")
+        arrow4.style.transform = "rotate(180deg)";
+        }
+}
+item4.addEventListener("click", toggleMenu4);
+
+const item5 = document.querySelector(".priceandarrow5");
+const description5 = document.querySelector(".description5");
+const arrow5 = document.querySelector(".arrow5");
+function toggleMenu5() {
+    if (description5.classList.contains("showMenu")) {
+    description5.classList.remove("showMenu")
+    arrow5.style.transform = "rotate(0deg)";
+    }   else {
+        description5.classList.add("showMenu")
+        arrow5.style.transform = "rotate(180deg)";
+        }
+}
+item5.addEventListener("click", toggleMenu5);
+
+const item6 = document.querySelector(".priceandarrow6");
+const description6 = document.querySelector(".description6");
+const arrow6 = document.querySelector(".arrow6");
+function toggleMenu6() {
+    if (description6.classList.contains("showMenu")) {
+    description6.classList.remove("showMenu")
+    arrow6.style.transform = "rotate(0deg)";
+    }   else {
+        description6.classList.add("showMenu")
+        arrow6.style.transform = "rotate(180deg)";
+        }
+}
+item6.addEventListener("click", toggleMenu6);
+
+const item7 = document.querySelector(".priceandarrow7");
+const description7 = document.querySelector(".description7");
+const arrow7 = document.querySelector(".arrow7");
+function toggleMenu7() {
+    if (description7.classList.contains("showMenu")) {
+    description7.classList.remove("showMenu")
+    arrow7.style.transform = "rotate(0deg)";
+    }   else {
+        description7.classList.add("showMenu")
+        arrow7.style.transform = "rotate(180deg)";
+        }
+}
+item7.addEventListener("click", toggleMenu7);
