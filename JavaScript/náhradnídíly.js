83c60aca97a8c2ea45571f99934c87416b625642
Vladislav Levitskii <vladlevitskii@gmail.com>
2023-05-25 18:15:25 +0200

    18:15

--- JavaScript/náhradnídíly.js
+++ JavaScript/náhradnídíly.js
@@ -96,3 +96,45 @@ function toggleMenu7() {
         }
 }
 item7.addEventListener("click", toggleMenu7);
+
+const item8 = document.querySelector(".priceandarrow8");
+const description8 = document.querySelector(".description8");
+const arrow8 = document.querySelector(".arrow8");
+function toggleMenu8() {
+    if (description8.classList.contains("showMenu")) {
+    description8.classList.remove("showMenu")
+    arrow8.style.transform = "rotate(0deg)";
+    }   else {
+        description8.classList.add("showMenu")
+        arrow8.style.transform = "rotate(180deg)";
+        }
+}
+item8.addEventListener("click", toggleMenu8);
+
+const item9 = document.querySelector(".priceandarrow9");
+const description9 = document.querySelector(".description9");
+const arrow9 = document.querySelector(".arrow9");
+function toggleMenu9() {
+    if (description9.classList.contains("showMenu")) {
+    description9.classList.remove("showMenu")
+    arrow9.style.transform = "rotate(0deg)";
+    }   else {
+        description9.classList.add("showMenu")
+        arrow9.style.transform = "rotate(180deg)";
+        }
+}
+item9.addEventListener("click", toggleMenu9);
+
+const item10 = document.querySelector(".priceandarrow10");
+const description10 = document.querySelector(".description10");
+const arrow10 = document.querySelector(".arrow10");
+function toggleMenu10() {
+    if (description10.classList.contains("showMenu")) {
+    description10.classList.remove("showMenu")
+    arrow10.style.transform = "rotate(0deg)";
+    }   else {
+        description10.classList.add("showMenu")
+        arrow10.style.transform = "rotate(180deg)";
+        }
+}
+item10.addEventListener("click", toggleMenu10);
